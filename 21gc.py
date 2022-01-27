a9cf1e5f95986bd56e95e7927a33f458da791be0
Majken <mohorton@ucdavis.edu>
2022-01-27 13:24:27 -0800

    first commit

---
+++ 21gc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Write a program that computes the GC% of a DNA sequence
+# Format the output for 2 decimal places
+# Use all three formatting methods
+
+dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT' # feel free to change
+count = 0
+for i in range(len(dna)):
+	if dna[i] == 'G':   count += 1
+	elif dna[i] == 'C': count += 1
+frac= count/len(dna)
+
+#Method 1 - old school print style
+print('%.2f' % (frac))
+#Method 2 - str. format
+print('{:.2f}' .format(frac))
+#Method 3 - f strings
+print(f'{count/len(dna):.2f}')
+
+"""
+python3 21gc.py
+0.42
+0.42
+0.42
+"""
