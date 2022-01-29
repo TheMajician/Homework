aa6d271218131cbef95d1a3d8061b96af4b8d452
Majken <mohorton@ucdavis.edu>
2022-01-28 17:21:29 -0800

    first commit

---
+++ 23anti.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Write a program that prints the reverse-complement of a DNA sequence
+# You must use a loop and conditional
+
+dna = 'ACTGAAAAAAAAAAA' [::-1]
+rc = ''
+for i in range(len(dna)):
+	nt = dna[i]
+	if nt == 'A': rc += 'T'
+	elif nt == 'T': rc += 'A'
+	elif nt == 'G': rc += 'C'
+	else: rc += 'G'
+	
+print(rc)
+
+"""
+python3 23anti.py
+TTTTTTTTTTTCAGT
+"""
