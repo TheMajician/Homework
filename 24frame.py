1274a8db6d8e621586da63c6e6596ebd42802940
Majken <mohorton@ucdavis.edu>
2022-01-28 17:22:30 -0800

    first commit

---
+++ 24frame.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Write a program that prints out the position, frame, and letter of the DNA
+# Try coding this with a single loop
+# Try coding this with nested loops
+
+dna = 'ATGGCCTTT'
+
+for position in range(len(dna)):   #don't know how to format this. end =' ' ????
+	print(position, end =' ')
+	if (position+1) % 2 == 0: print(1, end =' ')
+	elif (position+1) % 3 == 0: print(2, end =' ')
+	else: print(0, end =' ')
+	print(dna[position])
+
+#for frame in range(3):
+	#print(frame)
+		
+	#nt = dna[position]
+	#print(nt)
+"""	
+for i in range(len(dna)):
+	print(i)
+"""	
+"""
+python3 24frame.py
+0 0 A
+1 1 T
+2 2 G
+3 0 G
+4 1 C
+5 2 C
+6 0 T
+7 1 T
+8 2 T
+"""
