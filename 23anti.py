c8943535b8386df8c6bc76c76288f914d660584d
Majken <mohorton@ucdavis.edu>
2022-02-01 15:00:36 -0800

    class notes

--- 23anti.py
+++ 23anti.py
@@ -5,8 +5,9 @@
 
 dna = 'ACTGAAAAAAAAAAA' [::-1]
 rc = ''
-for i in range(len(dna)):
-	nt = dna[i]
+for i in range(len(dna)):     
+	nt = dna[i]                   		
+	                                 	
 	if nt == 'A': rc += 'T'
 	elif nt == 'T': rc += 'A'
 	elif nt == 'G': rc += 'C'
@@ -15,6 +16,21 @@ for i in range(len(dna)):
 print(rc)
 
 #worked with Inglis, Kim, Iris
+
+#===========================================
+#class notes
+	#range(len(dna)-1, -1, -1)
+	# or use backwards = len(dna)-1-1
+	# for nt in dna[::-1] print(nt):   if else etc
+	
+rev =''
+for i in range(len(dna)-1, -1, -1): #still not backwards....
+	nt = dna[i]
+	if   nt == 'A': rev += 'T'
+	elif nt == 'T': rev += 'A'
+	elif nt == 'C': rev += 'G'
+	else:           rev += 'C'
+print(rev)
 
 """
 python3 23anti.py
