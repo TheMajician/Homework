b5e9f82ec03bfd3e32c503abecf918f841f1f148
Majken <mohorton@ucdavis.edu>
2022-02-08 16:32:50 -0800

    inital commit

---
+++ 04dnd.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import random
+
+# 04 Element Adept
+
+trails = 1000000
+
+norm = 0
+adep = 0
+
+for i in range(trails):
+	r = random.randint(1, 10)
+	norm += r
+	if r == 1: r == 2
+	adep += r
+	
+normavg = norm/trails
+adepavg = adep/trails
+diff = adepavg - normavg
+print(f'Normal\tAdept\tDifference')	
+print(f'{normavg}\t{adepavg}\t{diff:.4f}')
+	
+#not giving different values.....?
