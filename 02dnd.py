d4a6aa54c6ffa74dab5142e18bc6cf5bc6b0046b
Majken <mohorton@ucdavis.edu>
2022-02-11 16:07:53 -0800

    update

--- 02dnd.py
+++ 02dnd.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python3
 import random
 
-rolls = 20
-fail = 0
-suc = 0
+trials = 5
 die = 0
 sta = 0
 rev = 0
 
-print(f'Number of Rolls\tDeath\tStablize\tRevive')
-for i in range(1, rolls+1):
-	r = random.randint(1, 20)
-	if   r == 1 : fail += 2
-	elif r < 10: fail += 1
-	elif r == 20 : rev +=1
-	elif r >= 10 : suc += 1
+for i in range(trials):
+	fail = 0
+	suc = 0
+	while True:
+		r = random.randint(1, 20)
 	
-	if fail == 3: die += 1
-	if suc == 3: sta += 1
-	print(f'{i}\t{die/rolls}\t{sta/rolls}\t{rev/rolls}') 
+		if   r == 1 : fail += 2
+		elif r < 10 : fail += 1
+		elif r >= 10: suc += 1
+		else:  
+			rev += 1
+			print('revived')
+			break
+		if fail >= 3:
+			die += 1
+			print("died")
+			break
+		if suc >= 3:
+			sta += 1
+			print("stablized")
+			break
+print(die, sta, rev) # not stopping loop and not giving an output
 
-#not giving correct probabilities...
-	
 	
 		
