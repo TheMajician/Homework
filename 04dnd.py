b60995d459370bbe64b331802f2ba566ce91185d
Majken <mohorton@ucdavis.edu>
2022-02-11 16:08:03 -0800

    update

--- 04dnd.py
+++ 04dnd.py
@@ -4,7 +4,7 @@ import random
 
 # 04 Element Adept
 
-trails = 1000000
+trails = 1000
 
 norm = 0
 adep = 0
@@ -12,7 +12,7 @@ adep = 0
 for i in range(trails):
 	r = random.randint(1, 10)
 	norm += r
-	if r == 1: r == 2
+	if r == 1: r = 2
 	adep += r
 	
 normavg = norm/trails
