ca915445221521714b545a8050537de2bec4f93d
Majken <mohorton@ucdavis.edu>
2022-02-21 14:41:08 -0800

    inital commit partial work

---
+++ 40transmembrane.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Write a program that predicts if a protein is trans-membrane
+# Trans-membrane proteins have the following properties
+#	Signal peptide: https://en.wikipedia.org/wiki/Signal_peptide
+#	Hydrophobic regions(s): https://en.wikipedia.org/wiki/Transmembrane_protein
+#	No prolines in hydrophobic regions (alpha helix)
+# Hydrophobicity is measued via Kyte-Dolittle
+#	https://en.wikipedia.org/wiki/Hydrophilicity_plot
+# For our purposes:
+#	Signal peptide is 8 aa long, KD > 2.5, first 30 aa
+#	Hydrophobic region is 11 aa long, KD > 2.0, after 30 aa
+# Hints:
+#   Create a function for KD calculation
+#   Create a function for amphipathic alpha-helix
+
+
+# Step One: read files
+filename = sys.argv[1]
+def readfasta(filename):
+	records = []
+	seq = ''
+	with open(filename) as fp:
+		for line in fp.readlines():
+			line = line.rstrip()
+			if line[0] == ">":
+				words = line.split()
+				id = words[0][1:]
+				#ids.append(words[0][1:])
+				if seq != '': 
+					records.append((id, seq))
+				seq = ''
+			else: 
+				seq += line
+		records.append((id, seq))
+	return records
+
+#stuff = readfasta(filename)
+#print(stuff)
+
+for id, seq in readfasta(filename):
+	print(id, seq)
+
+"""
+python3 40transmembrane.py ../Data/at_prots.fa
+AT1G75120.1
+AT1G10950.1
+AT1G75110.1
+AT1G74790.1
+AT1G12660.1
+AT1G75130.1
+"""
